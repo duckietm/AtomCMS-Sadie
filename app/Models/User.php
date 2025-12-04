45e2d4adac662fa7be94b02eda4fbcf00838af56
duckietm <1917443+duckietm@users.noreply.github.com>
2025-12-04 07:47:03 +0000

    style: fix code style with Laravel Pint

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,8 +48,10 @@ class User extends Authenticatable implements FilamentUser, HasName
     public $timestamps = false;
 
     protected $guarded = ['id'];
+
     protected $hidden = ['id', 'password', 'remember_token'];
-	protected $table = 'players';
+
+    protected $table = 'players';
 
     protected function casts(): array
     {
