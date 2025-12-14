358675028fb8c74a17c44c6a14f7d73d4098a5d0
DuckieTM <info@camwijs.nl>
2025-12-14 13:43:34 +0100

    Update 2022_08_14_210602_add_hidden_staff_to_users_table.php

--- database/migrations/2022_08_14_210602_add_hidden_staff_to_users_table.php
+++ database/migrations/2022_08_14_210602_add_hidden_staff_to_users_table.php
@@ -8,12 +8,40 @@ return new class extends Migration
 {
     public function up(): void
     {
+        if (!Schema::hasTable('roles')) {
+            Schema::create('roles', function (Blueprint $table) {
+                $table->increments('id');
+                $table->longText('name');
+                $table->boolean('hidden_staff')->default(false);
+                $table->boolean('hidden_rank')->default(false);
+                $table->string('job_description', 255)->default('Here to help');
+                $table->string('staff_color', 8)->default('#327fa8');
+                $table->string('staff_background', 255)->default('staff-bg.png');
+            });
+        }
+
         Schema::table('roles', function (Blueprint $table) {
-            if (columnExists('roles', 'hidden_staff')) {
-                Schema::dropColumns('roles', 'hidden_staff');
+
+            if (Schema::hasColumn('roles', 'hidden_staff')) {
+                $table->dropColumn('hidden_staff');
             }
 
-            $table->boolean('hidden_staff')->after('name')->default(false);
+            $table->boolean('hidden_staff')
+                ->after('name')
+                ->default(false);
+
+            if (Schema::hasColumn('roles', 'hidden_rank')) {
+                $table->dropColumn('hidden_rank');
+            }
+
+            $table->boolean('hidden_rank')
+                ->after('hidden_staff')
+                ->default(false);
         });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
     }
 };
